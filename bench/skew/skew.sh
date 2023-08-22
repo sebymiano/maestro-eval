7dd2d9cdacec095ed68cc6474bb452597541114b
Francisco Pereira <fchamicapereira@gmail.com>
2023-08-22 11:40:44 +0100

    Backup

--- bench/skew/skew.sh
+++ bench/skew/skew.sh
@@ -15,8 +15,8 @@ NF="fw"
 
 main() {
     for ((skew_it=0;skew_it<$SKEW_ITERATIONS;skew_it++)); do
-        #bench_nf "skew-$NF-uniform-$skew_it" "$NF" "$TARGET" "$UNIFORM_PCAP" "$CURRENT_EXPERIMENT_DIR" "skew-$NF-uniform-$skew_it"
-        #bench_nf "skew-$NF-zipf-$skew_it" "$NF" "$TARGET" "$ZIPF_PCAP" "$CURRENT_EXPERIMENT_DIR" "skew-$NF-zipf-$skew_it"
+        bench_nf "skew-$NF-uniform-$skew_it" "$NF" "$TARGET" "$UNIFORM_PCAP" "$CURRENT_EXPERIMENT_DIR" "skew-$NF-uniform-$skew_it"
+        bench_nf "skew-$NF-zipf-$skew_it" "$NF" "$TARGET" "$ZIPF_PCAP" "$CURRENT_EXPERIMENT_DIR" "skew-$NF-zipf-$skew_it"
         bench_balanced_nf "skew-$NF-zipf-balanced-$skew_it" "$NF" "$TARGET" "$ZIPF_PCAP" "$CURRENT_EXPERIMENT_DIR" "skew-$NF-zipf-balanced-$skew_it"
 	done
 }
