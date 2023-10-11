9800a1e10febb06796fc0d6a0d43ef8e44925b9e
Francisco Pereira <fchamicapereira@gmail.com>
2023-10-11 15:10:56 +0200

    Generalized vars file

--- bench/vars.sh
+++ bench/vars.sh
@@ -7,16 +7,16 @@ SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 DATA_DIR="$SCRIPT_DIR/data"
 PCAPS_DIR="$SCRIPT_DIR/pcaps"
 
-DUT=geodude
-TG=graveler
+DUT="" # FIXME: DUT ssh entry
+TG=""  # FIXME: TG ssh entry
 
-DUT_EVAL_DIR=~/maestro-eval
-TG_EVAL_DIR=~/maestro-eval
+DUT_EVAL_DIR=~/maestro-eval # Path to this repo on the DUT
+TG_EVAL_DIR=~/maestro-eval  # Path to this repo on the TG
 
-DUT_CORES="16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31"
+DUT_CORES="" # FIXME: comma separated list of cores to be used (e.g. "0,1,2,3")
 
-TG_TX_DEV="0000:af:00.1"
-TG_RX_DEV="0000:af:00.0"
+TG_TX_DEV="" # FIXME: TX PCIe device on the TG
+TG_RX_DEV="" # FIXME: RX PCIe device on the TG
 
 TG_TX_CORES=6
 TG_RX_CORES=6
