f0e175c82ac72593d1f89dc80331a1f1f0240c8c
Francisco Pereira <fchamicapereira@gmail.com>
2023-12-25 11:09:06 +0100

    Dumb mistakes

--- util/setup-dut.sh
+++ util/setup-dut.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MAESTRO_EVAL_DIR="~/maestro-eval"
+MAESTRO_EVAL_DIR=~/maestro-eval
 
 source $MAESTRO_EVAL_DIR/bench/vars.sh
 
@@ -10,5 +10,5 @@ sudo insmod $MAESTRO_EVAL_DIR/build/dpdk-kmods/linux/igb_uio/igb_uio.ko
 sudo dpdk-hugepages.py --node 0 --reserve 42G
 sudo dpdk-hugepages.py --node 1 --reserve 42G
 
-sudo -E $MAESTRO_EVAL_DIR/util/bind-igb-uio.sh $TG_TX_DEV
-sudo -E $MAESTRO_EVAL_DIR/util/bind-igb-uio.sh $TG_RX_DEV
+sudo -E $MAESTRO_EVAL_DIR/util/bind-igb-uio.sh $DUT_TX_DEV
+sudo -E $MAESTRO_EVAL_DIR/util/bind-igb-uio.sh $DUT_RX_DEV
