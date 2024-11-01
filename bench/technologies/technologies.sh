82e658450ca5ca68eb45663dfc3b5623e4cbb936
Sebastiano Miano <mianosebastiano@gmail.com>
2024-11-01 17:20:39 +0000

    Modify technologies.sh script

--- bench/technologies/technologies.sh
+++ bench/technologies/technologies.sh
@@ -23,10 +23,10 @@ locks() {
     bench_balanced_nf "nop-locks" "nop" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "nop-locks"
     bench_balanced_nf "pol-locks" "pol" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "pol-locks"
     bench_balanced_nf "sbridge-locks" "sbridge" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "sbridge-locks"
-    bench_balanced_nf "bridge-locks" "bridge" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "bridge-locks"
+    # bench_balanced_nf "bridge-locks" "bridge" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "bridge-locks"
     bench_balanced_nf "fw-locks" "fw" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "fw-locks"
     bench_balanced_nf "nat-locks" "nat" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "nat-locks"
-    bench_balanced_lb "lb-locks" "lb" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "lb-locks"
+    # bench_balanced_lb "lb-locks" "lb" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "lb-locks"
     bench_balanced_nf "psd-locks" "psd" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "psd-locks"
     bench_balanced_nf "cl-locks" "cl" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "cl-locks"
 }
@@ -35,14 +35,28 @@ tm() {
     bench_balanced_nf "nop-tm" "nop" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "nop-tm"
     bench_balanced_nf "pol-tm" "pol" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "pol-tm"
     bench_balanced_nf "sbridge-tm" "sbridge" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "sbridge-tm"
-    bench_balanced_nf "bridge-tm" "bridge" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "bridge-tm"
+    # bench_balanced_nf "bridge-tm" "bridge" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "bridge-tm"
     bench_balanced_nf "fw-tm" "fw" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "fw-tm"
     bench_balanced_nf "nat-tm" "nat" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "nat-tm"
-    bench_balanced_lb "lb-tm" "lb" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "lb-tm"
+    # bench_balanced_lb "lb-tm" "lb" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "lb-tm"
     bench_balanced_nf "psd-tm" "psd" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "psd-tm"
     bench_balanced_nf "cl-tm" "cl" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "cl-tm"
 }
 
+seq() {
+    bench_nf "nop-seq" "nop" "seq" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "nop-seq"
+    bench_nf "pol-seq" "pol" "seq" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "pol-seq"
+    bench_nf "sbridge-seq" "sbridge" "seq" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "sbridge-seq"
+    # bench_nf "bridge-seq" "bridge" "seq" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "bridge-seq"
+    bench_nf "fw-seq" "fw" "seq" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "fw-seq"
+    bench_nf "nat-seq" "nat" "seq" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "nat-seq"
+    # bench_balanced_lb "lb-seq" "lb" "seq" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "lb-seq"
+    bench_nf "psd-seq" "psd" "seq" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "psd-seq"
+    bench_nf "cl-seq" "cl" "seq" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "cl-seq"
+}
+
+seq
 shared_nothing
 locks
 tm
+
