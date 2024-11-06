111dc1cd3f7323da098e924502d8b9d5e7e5f982
Sebastiano Miano <mianosebastiano@gmail.com>
2024-11-06 08:45:05 +0000

    Update test scripts

---
+++ bench/technologies/technologies-imc-scr.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -euo pipefail
+
+CURRENT_EXPERIMENT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+FUNCTIONS_FILE="$CURRENT_EXPERIMENT_DIR/../functions.sh"
+source $FUNCTIONS_FILE
+
+PCAP="imc_scr_64B.pcap"
+
+shared_nothing() {
+    bench_balanced_nf "nop-sn" "nop" "sn" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "nop-sn-single"
+    bench_balanced_nf "cl-sn" "cl" "sn" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "cl-sn-single"
+    bench_balanced_nf "pol-sn" "pol" "sn" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "pol-sn-single"
+    bench_balanced_nf "sbridge-sn" "sbridge" "sn" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "sbridge-sn-single"
+    bench_balanced_nf "fw-sn" "fw" "sn" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "fw-sn-single"
+    bench_balanced_nf "nat-sn" "nat" "sn" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "nat-sn-single"
+    bench_balanced_nf "psd-sn" "psd" "sn" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "psd-sn-single"
+}
+
+locks() {
+    bench_balanced_nf "nop-locks" "nop" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "nop-locks"
+    bench_balanced_nf "pol-locks" "pol" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "pol-locks"
+    bench_balanced_nf "sbridge-locks" "sbridge" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "sbridge-locks"
+    bench_balanced_nf "fw-locks" "fw" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "fw-locks"
+    bench_balanced_nf "nat-locks" "nat" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "nat-locks"
+    bench_balanced_nf "psd-locks" "psd" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "psd-locks"
+    bench_balanced_nf "cl-locks" "cl" "locks" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "cl-locks"
+}
+
+tm() {
+    bench_balanced_nf "nop-tm" "nop" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "nop-tm"
+    bench_balanced_nf "pol-tm" "pol" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "pol-tm"
+    bench_balanced_nf "sbridge-tm" "sbridge" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "sbridge-tm"
+    bench_balanced_nf "fw-tm" "fw" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "fw-tm"
+    bench_balanced_nf "nat-tm" "nat" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "nat-tm"
+    bench_balanced_nf "psd-tm" "psd" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "psd-tm"
+    bench_balanced_nf "cl-tm" "cl" "tm" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "cl-tm"
+}
+
+seq() {
+    bench_nf "nop-seq" "nop" "seq" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "nop-seq"
+    bench_nf "pol-seq" "pol" "seq" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "pol-seq"
+    bench_nf "sbridge-seq" "sbridge" "seq" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "sbridge-seq"
+    bench_nf "fw-seq" "fw" "seq" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "fw-seq"
+    bench_nf "nat-seq" "nat" "seq" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "nat-seq"
+    bench_nf "psd-seq" "psd" "seq" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "psd-seq"
+    bench_nf "cl-seq" "cl" "seq" "$PCAP" "$CURRENT_EXPERIMENT_DIR" "cl-seq"
+}
+
+# seq
+shared_nothing
+# locks
+# tm
+
---
+++ scr-nfs/run_tests_imc_scr.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+set -euo pipefail
+
+CURRENT_EXPERIMENT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+# set -x
+FUNCTIONS_FILE="$CURRENT_EXPERIMENT_DIR/../bench/functions.sh"
+source $FUNCTIONS_FILE
+
+DUT_SCR_PCAPS_DIR=$DUT_EVAL_DIR/scr-nfs/pcaps
+TG_SCR_PCAPS_DIR=$TG_EVAL_DIR/scr-nfs/pcaps
+
+PCAP_FOLDER_CL="cl_imc_scr_64_scr"
+PCAP_FOLDER_FW="fw_imc_scr_64_scr"
+PCAP_FOLDER_NAT="nat_imc_scr_64_scr"
+PCAP_FOLDER_SBRIDGE="sbridge_imc_scr_64_scr"
+PCAP_FOLDER_NOP="nop_imc_scr_64_scr"
+PCAP_FOLDER_PSD="psd_imc_scr_64_scr"
+PCAP_FOLDER_POL="pol_imc_scr_64_scr"
+
+DUT_SCR_DIR=${DUT_EVAL_DIR}/scr-nfs
+
+build_nf_scr() {
+	local nf_exe=$1
+
+	local nf_src="$nf_exe.c"
+
+	dut_run "mkdir -p $DUT_SYNTHESIZED_DIR"
+	dut_run "rm $DUT_SYNTHESIZED_DIR/${nf_exe} 2> /dev/null || true"
+	dut_run "cp $DUT_SCR_DIR/$nf_src $DUT_SYNTHESIZED_DIR/${nf_src}"
+
+	dut_run "SRC=$nf_src make -f $DUT_DPDK_MAKEFILE" $DUT_SYNTHESIZED_DIR >> $CURRENT_LOG 2>&1
+}
+
+run_balanced_bench_scr() {
+	local nf_exe=$1
+	local target=$2
+    local pcap_pattern=$3
+	local exp_dir=$4
+	local exp_name=$5
+
+	local intermediate_results_file="$exp_dir/.imc_scr.csv"
+	local tmp_results_file="$exp_dir/.results.csv"
+	local results_file="$exp_dir/$exp_name.csv"
+
+	__setup_bench "$nf_exe" "$tmp_results_file"
+
+	local MIN_CORES=1
+	local MAX_CORES=$(python3 -c "print(len('$DUT_CORES'.split(',')))")
+
+	for ((n_cores=$MIN_CORES;n_cores<=$MAX_CORES;n_cores++)); do
+        local pcap_file="${pcap_pattern}${n_cores}cores.pcap"
+
+		if [ "$target" == "cl" ]; then
+			export TG_PCAPS_DIR=${TG_SCR_PCAPS_DIR}/${PCAP_FOLDER_CL}
+			export DUT_PCAPS_DIR=${DUT_SCR_PCAPS_DIR}/${PCAP_FOLDER_CL}
+		elif [ "$target" == "sbridge" ]; then
+			export TG_PCAPS_DIR=${TG_SCR_PCAPS_DIR}/${PCAP_FOLDER_SBRIDGE}
+			export DUT_PCAPS_DIR=${DUT_SCR_PCAPS_DIR}/${PCAP_FOLDER_SBRIDGE}
+		elif [ "$target" == "fw" ]; then
+			export TG_PCAPS_DIR=${TG_SCR_PCAPS_DIR}/${PCAP_FOLDER_FW}
+			export DUT_PCAPS_DIR=${DUT_SCR_PCAPS_DIR}/${PCAP_FOLDER_FW}
+		elif [ "$target" == "nat" ]; then
+			export TG_PCAPS_DIR=${TG_SCR_PCAPS_DIR}/${PCAP_FOLDER_NAT}
+			export DUT_PCAPS_DIR=${DUT_SCR_PCAPS_DIR}/${PCAP_FOLDER_NAT}
+		elif [ "$target" == "nop" ]; then
+			export TG_PCAPS_DIR=${TG_SCR_PCAPS_DIR}/${PCAP_FOLDER_NOP}
+			export DUT_PCAPS_DIR=${DUT_SCR_PCAPS_DIR}/${PCAP_FOLDER_NOP}
+		elif [ "$target" == "psd" ]; then
+			export TG_PCAPS_DIR=${TG_SCR_PCAPS_DIR}/${PCAP_FOLDER_PSD}
+			export DUT_PCAPS_DIR=${DUT_SCR_PCAPS_DIR}/${PCAP_FOLDER_PSD}
+		elif [ "$target" == "pol" ]; then
+			export TG_PCAPS_DIR=${TG_SCR_PCAPS_DIR}/${PCAP_FOLDER_POL}
+			export DUT_PCAPS_DIR=${DUT_SCR_PCAPS_DIR}/${PCAP_FOLDER_POL}
+		else
+			echo "Error: Unknown target '$target'."
+			return 1
+		fi
+		
+        tg_check_file "${TG_PCAPS_DIR}/${pcap_pattern}${n_cores}cores.pcap"
+		__run_balanced_bench_with_n_cores "$nf_exe" "$pcap_file" "$n_cores" "$intermediate_results_file" "$tmp_results_file" "$exp_name"
+	done
+
+	__finalize_bench "$tmp_results_file" "$results_file"
+}
+
+bench_balanced_nf_scr() {
+	local nf_exe=$1
+	local target=$2
+	local pcap_pattern=$3
+	local exp_dir=$4
+	local exp_name=$5
+
+	set_log "$exp_dir"
+	build_nf_scr "$nf_exe" $target
+	run_balanced_bench_scr "$nf_exe" "$target" "$pcap_pattern" "$exp_dir" "$exp_name"
+}
+
+state_compute_replication() {
+	bench_balanced_nf_scr "sbridge-scr" "sbridge" "dpdk_sbridge_scr_" "$CURRENT_EXPERIMENT_DIR" "sbridge-scr-imc_scr"
+    bench_balanced_nf_scr "cl-scr" "cl" "dpdk_cl_scr_" "$CURRENT_EXPERIMENT_DIR" "cl-scr-imc_scr"
+	bench_balanced_nf_scr "fw-scr" "fw" "dpdk_fw_scr_" "$CURRENT_EXPERIMENT_DIR" "fw-scr-imc_scr"
+	bench_balanced_nf_scr "nat-scr" "nat" "dpdk_nat_scr_" "$CURRENT_EXPERIMENT_DIR" "nat-scr-imc_scr"
+	bench_balanced_nf_scr "nop-scr" "nop" "dpdk_nop_scr_" "$CURRENT_EXPERIMENT_DIR" "nop-scr-imc_scr"
+	bench_balanced_nf_scr "psd-scr" "psd" "dpdk_psd_scr_" "$CURRENT_EXPERIMENT_DIR" "psd-scr-imc_scr"
+	bench_balanced_nf_scr "pol-scr" "pol" "dpdk_pol_scr_" "$CURRENT_EXPERIMENT_DIR" "pol-scr-imc_scr"
+}
+
+state_compute_replication
