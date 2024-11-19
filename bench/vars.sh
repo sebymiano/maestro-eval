6da84b2e5282b43ce5a3d7f00f4469c2e5d4d9cd
smiano <smiano@dut.test-ton.morpheus-pg0.wisc.cloudlab.us>
2024-11-19 11:10:54 +0000

    Update vars

--- bench/vars.sh
+++ bench/vars.sh
@@ -24,7 +24,7 @@ DUT_CORES="1,2,3,4,5,6,7,8" # FIXME: comma separated list of cores to be used (e
 # DUT_CORES="1,3"
 
 DUT_SCR_DIR=${DUT_EVAL_DIR}/scr-nfs/nf-no-expire
-DUT_MLNX_SYNT_DIR=${DUT_EVAL_DIR}/synthesized-mlnx/nf-no-expire
+DUT_MLNX_SYNT_DIR=${DUT_EVAL_DIR}/synthesized-mlnx/nf-no-expire-tx-queues
 
 TG_TX_DEV="0000:51:00.0" # FIXME: TX PCIe device on the TG
 TG_RX_DEV="0000:51:00.1" # FIXME: RX PCIe device on the TG
