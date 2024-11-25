589bad4a8f7f10531d29f203799c2ac60d064f1d
smiano <smiano@dut.test-ton.morpheus-pg0.wisc.cloudlab.us>
2024-11-25 12:12:06 +0000

    Update installers

--- installers/installers.sh
+++ installers/installers.sh
@@ -71,11 +71,6 @@ setup_docker() {
 	sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 	sudo apt-get update
 	sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
-
-	sudo groupadd docker || true
-	sudo usermod -aG docker $USER
-	newgrp docker
-	docker run hello-world
 }
 
 install_doca() {
