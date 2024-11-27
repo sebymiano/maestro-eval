24c737be4e3abf56b2f46585818f31f33b507c5a
Sebastiano Miano <mianosebastiano@gmail.com>
2024-11-27 15:28:03 +0000

    Use 1G hugepages

--- util/setup-dut.sh
+++ util/setup-dut.sh
@@ -8,8 +8,8 @@ source $MAESTRO_EVAL_DIR/bench/vars.sh
 sudo modprobe uio
 sudo insmod $MAESTRO_EVAL_DIR/build/dpdk-kmods/linux/igb_uio/igb_uio.ko
 
-sudo $MAESTRO_EVAL_DIR/build/dpdk/usertools/dpdk-hugepages.py --node 0 --reserve 10G
-sudo $MAESTRO_EVAL_DIR/build/dpdk/usertools/dpdk-hugepages.py --node 1 --reserve 10G
+sudo $MAESTRO_EVAL_DIR/build/dpdk/usertools/dpdk-hugepages.py --node 0 --reserve 10G --pagesize 1G
+sudo $MAESTRO_EVAL_DIR/build/dpdk/usertools/dpdk-hugepages.py --node 1 --reserve 10G --pagesize 1G
 
 sudo -E $MAESTRO_EVAL_DIR/util/bind-igb-uio.sh $DUT_TX_DEV
 sudo -E $MAESTRO_EVAL_DIR/util/bind-igb-uio.sh $DUT_RX_DEV
