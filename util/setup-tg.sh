21c6ac945a04cb1ab12e6636133f053cd9cb849b
Sebastiano Miano <mianosebastiano@gmail.com>
2024-11-27 15:24:56 +0000

    Update setup-tg script

--- util/setup-tg.sh
+++ util/setup-tg.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
-MAESTRO_EVAL_DIR=~/maestro-eval
+MAESTRO_EVAL_DIR=$SCRIPT_DIR/..
 
 source $MAESTRO_EVAL_DIR/bench/vars.sh
 
 sudo modprobe uio
 sudo insmod $MAESTRO_EVAL_DIR/build/dpdk-kmods/linux/igb_uio/igb_uio.ko
 
-sudo dpdk-hugepages.py --node 0 --reserve 42G
-sudo dpdk-hugepages.py --node 1 --reserve 42G
+sudo $MAESTRO_EVAL_DIR/build/dpdk/usertools/dpdk-hugepages.py --node 0 --reserve 42G
+sudo $MAESTRO_EVAL_DIR/build/dpdk/usertools/dpdk-hugepages.py --node 1 --reserve 42G
 
 sudo -E $MAESTRO_EVAL_DIR/util/bind-igb-uio.sh $TG_TX_DEV
 sudo -E $MAESTRO_EVAL_DIR/util/bind-igb-uio.sh $TG_RX_DEV
